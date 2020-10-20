28270ccec13408b7d6f4d1b0e1f1ab7e0d969639
Aseem <aseemanand2@gmail.com>
2020-10-20 22:51:57 +0530

    [Aseem] Created new project to find max value

---
+++ FindMaximumProblemUsingGeneric/Program.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace FindMaximumProblemUsingGeneric
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
